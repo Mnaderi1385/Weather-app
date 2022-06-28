f06504554b61e23ec8d5420f323c7ec1fd47dff3
Mnaderi1385 <mo.naderi1385@gmail.com>
2022-06-27 18:05:40 -0700

    Update

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -21,23 +21,9 @@ const Weather = () => {
 
     const handleGetWeather = (e) => {
         e.preventDefault();
-        
-        if(cityQuery === '') {
-            toast.error('لطفا شهر خود را وارد کنید', {
-                style: {
-                    background: 'red',
-                    color: '#fff',
-                    fontWeight: 'bold',
-                    padding: '1.4rem',
-                },
-                position: 'bottom-center'
-            })
-        } else {
-            // dispatch(getWeatherInfo(query) );
-            dispatch(sendWeatherReq(cityQuery, latQuery, lonQuery));
-            setCityQuery('');
-        };
 
+        dispatch(sendWeatherReq(cityQuery, latQuery, lonQuery));
+        setCityQuery('');
         if(err) {
             toast.error('مشکلی پیش اومده لطفا دوباره تلاش کنید', {
                 style: {
@@ -71,7 +57,6 @@ const Weather = () => {
                     <h1 className={`text-lg sm:text-2xl font-bold ${bgMode === 'usual' || bgMode === 'cold' ? 'text-slate-700' : 'text-slate-100'} text-slate-100`}>وضعیت آب و هوا</h1>
                     
                     <form action="#" className="mt-24" onSubmit={handleGetWeather}>
-                        { console.log(data.coord) }
                         <input type="text" ref={input} placeholder={data.name || 'نام شهر ...'} className="w-full bg-transparent text-slate-700 text-sm p-3 outline-none border-slate-600 border-2 duration-200 font-bold placeholder:font-semibold placeholder:text-slate-700 rounded-md mt-2" value={cityQuery} onChange={(e) => setCityQuery(e.target.value)} />
                         <input type="number" placeholder={data.name ? data.coord.lon : 'طول جفرافیایی (اختیاری)'} className="w-full bg-transparent text-slate-700 text-sm p-3 outline-none border-slate-600 border-2 duration-200 font-bold placeholder:font-semibold placeholder:text-slate-700 rounded-md mt-2" value={lonQuery} onChange={(e) => setLonQuery(e.target.value)} />
                         <input type="number" placeholder={data.name ? data.coord.lat : 'عرض جفرافیایی (اختیاری)'} className="w-full bg-transparent text-slate-700 text-sm p-3 outline-none border-slate-600 border-2 duration-200 font-bold placeholder:font-semibold placeholder:text-slate-700 rounded-md mt-2" value={latQuery} onChange={(e) => setLatQuery(e.target.value)} />
@@ -95,8 +80,8 @@ const Weather = () => {
                             <h2 className="font-bold text-lg md:text-4xl text-white animate-pulse mt-5">{ data.name }</h2>
                             <h3 className="font-bold text-lg md:text-3xl text-gray-300 mt-5" dir="ltr">{ `${data.main.temp}  C°` }</h3>
                             <h3 className="font-bold text-base md:text-lg text-gray-400 my-5">{ data.weather[0].main }</h3>
-                            <span className="font-medium text-sm text-slate-700" dir="ltr">طول جغرافیایی: { data.coord.lon }</span><br /><br />
-                            <span className="font-medium text-sm text-slate-700" dir="ltr">عرض جغرافیایی: { data.coord.lat }</span>
+                            <span className="font-medium text-sm text-gray-300" dir="ltr">طول جغرافیایی: { data.coord.lon }</span><br /><br />
+                            <span className="font-medium text-sm text-gray-300" dir="ltr">عرض جغرافیایی: { data.coord.lat }</span>
                         </div>
                     ) : (
                         <div className="bg-slate-500/90 rounded-lg py-5 px-3 my-16 mx-auto">
